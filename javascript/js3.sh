0a30e397f601f3599f5e0b5d10bd255596c0b73d
Benjamin Raymond <benjamin.raymond@epitech.eu>
2021-02-10 18:58:34 +0100

    Fix js3 and js4

--- javascript/js3.sh
+++ javascript/js3.sh
@@ -2,5 +2,6 @@
 
 cd ../.files/js3/
 php -S localhost:8006 2>&1 | cat > /dev/null &
+php -S localhost:8000 login.php
 echo "Challenge url is http://localhost:8006/"
 firefox http://localhost:8006/
--- javascript/js4.sh
+++ javascript/js4.sh
@@ -2,5 +2,6 @@
 
 cd ../.files/js4/
 php -S localhost:8007 2>&1 | cat > /dev/null &
+php -S localhost:8000 login.php
 echo "Challenge url is http://localhost:8007/"
 firefox http://localhost:8007/
